f90aca662b6a7ea5c2441ac6e91494525caa1b7a
Ilja Fedorov <iliagus2@gmail.com>
2024-08-14 13:21:35 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,11 @@
+name = 'Илья'
+print('Имя:', name, type (name))
+age = 25
+print('Возраст:', age, type (age), id (age))
+age =age + 0.6
+print('Новый возраст:', age, type (age), id (age))
+is_student = True
+print('Студент:', is_student, type (is_student), id (is_student))
+
+#age = True
+#print('Возраст:', age, type (age), id (age))
