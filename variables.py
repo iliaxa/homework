483f0eb6ec24efe1711953d54fee95f0571763e7
Ilja Fedorov <iliagus2@gmail.com>
2024-08-14 13:38:31 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+number_of_compl_tasks = 12
+number_of_spent_hours = 1.5
+name_of_course = 'Python'
+time_to_complete_task = number_of_spent_hours / number_of_compl_tasks
+print(name_of_course,', всего задач:',number_of_compl_tasks, ', затрачено часов: ',number_of_spent_hours,', среднее время выполнения', time_to_complete_task, ' часа.', sep='')
