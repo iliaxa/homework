eca7a353e74945c5aabeaf615c29e08febed79bc
Ilja Fedorov <iliagus2@gmail.com>
2024-08-16 14:20:57 +0300

    Update module_1_6.py

--- dict_and_sets/module_1_6.py
+++ dict_and_sets/module_1_6.py
@@ -1,18 +1,19 @@
-my_dict = {'a': "азбука", 'b': ['boat', 'bear', 'beer'], 'c': 'словарь'}
-print(my_dict['a'])
+my_dict = {'A': "Aзбука", 'b': ['boat', 'bear', 'beer'], 'C': 'Cловарь'}
+print('Dict: ', my_dict)
+print('Буква А: ', my_dict['A'])
 my_dict['d'] = 4**4 
-print(my_dict['d'])  
+print('Value from New pair: ', my_dict['d'])  
 my_dict.update({'e': 4*4,
                 'f': 4+4})
 b = my_dict.pop('b')
-print(b)
-print(my_dict)
+print('Value of deleted key: ', b)
+print('Fresh dict: ', my_dict, '\n')
 
 my_set = {3, 'ready', 2, 'steady', 2, 'go', True, 'ready', False}
-print('\n', my_set)
+print('Unic set: ', my_set)
 # my_set.add('fire')
 # my_set.add('shoot')
 a = 'fire', 'shoot'
 my_set.update(a)
 my_set.discard(1) #1=True
-print(my_set)
+print('fresh unic set: ', my_set)
