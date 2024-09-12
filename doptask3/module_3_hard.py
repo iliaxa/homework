85ddb4ea3d24abedd4780f3cbff5021b844dcc4b
Ilja Fedorov <iliagus2@gmail.com>
2024-09-12 10:58:19 +0300

    Update module_3_hard.py

--- doptask3/module_3_hard.py
+++ doptask3/module_3_hard.py
@@ -1,3 +1,23 @@
+def calculate_structure_sum(*args):
+    structure_sum = 0
+    for arg in args:
+        if isinstance(arg, int):
+            structure_sum += arg
+        elif isinstance(arg, str):
+            structure_sum += len(arg)
+        elif isinstance(arg, dict):
+            for i in arg.items():
+                structure_sum += calculate_structure_sum(i)
+        elif isinstance(arg, tuple):
+            for i in arg:
+                structure_sum += calculate_structure_sum(i)
+        else:
+            structure_sum += calculate_structure_sum(*arg)
+        # print('elem-', arg, '\n summa=', structure_sum)
+    return structure_sum
+
+
+
 data_structure = [
 [1, 2, 3],
 {'a': 4, 'b': 5},
@@ -5,6 +25,5 @@ data_structure = [
 "Hello",
 ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
-
 result = calculate_structure_sum(data_structure)
 print(result)
