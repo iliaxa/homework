24a4f612bbdb130e9cf8d1d0637efad7a962cd38
Ilja Fedorov <iliagus2@gmail.com>
2024-10-09 12:25:40 +0300

    Update module_7_1.py

--- w_w_files/module_7_1.py
+++ w_w_files/module_7_1.py
@@ -1,12 +1,4 @@
-from pprint import pprint
-
 class Product:
-    """
-Атрибут name - название продукта (строка).
-Атрибут weight - общий вес товара (дробное число) (5.4, 52.8 и т.п.).
-Атрибут category - категория товара (строка).
-Метод __str__, который возвращает строку в формате '<название>, <вес>, <категория>'. Все данные в строке разделены запятой с пробелами.
-"""
     def __init__(self, name, weight, category):
         self.name = name
         self.weight = weight
@@ -16,15 +8,35 @@ class Product:
         return f'{self.name}, {self.weight}, {self.category}'
         
 
-# class Shop:
-    
-    
-#     def __init__(self):
-#         file_name = 'products.txt'
+class Shop:
+    __file_name = 'products.txt'
 
-#     def get_products(self):
-#         product_list = ''
-#         open(file_name, 'r')
-#         print(file_name.read())
+    def get_products(self):
+        product_list = ''
+        file = open(self.__file_name, 'r')
+        for i in file:
+            product_list += i
+        file.close()
+        return product_list
+
     
-#         file_name.close()
+    def add(self, *products):
+        file = open(self.__file_name, 'a')
+        for product in products:
+            if product.name not in self.get_products():
+                file.write(f'{product}\n')
+            else:
+                print(f'Продукт {product.name} уже есть в магазине')
+        file.close()
+    
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
