105d4687ae727f9ced28b5b9b80e1931cc67118d
Ilja Fedorov <iliagus2@gmail.com>
2024-10-09 19:57:14 +0300

    Update module_7_3.py

--- w_w_files/module_7_3.py
+++ w_w_files/module_7_3.py
@@ -4,19 +4,20 @@ def remove_punctuation(text):
         text = text.replace(p, '')
     return text
 
+
 class WordsFinder:
     def __init__(self, *file_names):
         self.file_names = list(file_names)
-        
+
     def get_all_words(self):
         all_words = {}
         for file_name in self.file_names:
-            with open(file_name, 'r') as file:
+            with open(file_name, 'r', encoding='utf-8') as file:
                 file_without_p = remove_punctuation(file.read().lower())
-                result = file_without_p.split() # бахнуть множеством
+                result = file_without_p.split()  # бахнуть множеством
                 all_words[file_name] = result
         return all_words
-        
+
     def find(self, word):
         all_words = self.get_all_words()
         result = {}
@@ -34,8 +35,9 @@ class WordsFinder:
         return result
 
 
-
-finder2 = WordsFinder('sample.txt', 'test.txt')
-print(finder2.get_all_words()) # Все слова
-print(finder2.find('TEXT')) # 3 слово по счёту
-print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+finder1 = WordsFinder('Walt Whitman - O Captain! My Captain!.txt',
+                      'Rudyard Kipling - If.txt',
+                      'Mother Goose - Monday’s Child.txt')
+print(finder1.get_all_words())
+print(finder1.find('the'))
+print(finder1.count('the'))
