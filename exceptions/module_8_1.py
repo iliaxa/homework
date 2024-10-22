de90f6081a25f720a7eaea08b2092100d0df112a
Ilja Fedorov <iliagus2@gmail.com>
2024-10-22 10:03:22 +0300

    Create module_8_1.py

---
+++ exceptions/module_8_1.py
@@ -0,0 +1,13 @@
+def add_everything_up(a, b):
+    try:
+        c = a + b
+    except TypeError:
+        print("You can't add a string and a number!")
+        return f'{a}{b}'
+    else:
+        return c
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
