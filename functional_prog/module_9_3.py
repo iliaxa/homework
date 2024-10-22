c934894704ca6ab9d25d91391dd7dc10d52c9489
Ilja Fedorov <iliagus2@gmail.com>
2024-10-22 17:53:39 +0300

    Create module_9_3.py
    доделать

---
+++ functional_prog/module_9_3.py
@@ -0,0 +1,5 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(s1) - len(s2) for s1, s2 in zip(first, second))
+second_result =  length_differences = (len(list1[i]) - len(list2[i]) for i in range(min(len(list1), len(list2)))) доделать дома(())))
