0b11beb8b0b6f9113a4e6e6ef4265c9a319d2ae2
Ilja Fedorov <iliagus2@gmail.com>
2024-10-24 16:37:06 +0300

    Update module_9_3.py

--- functional_prog/module_9_3.py
+++ functional_prog/module_9_3.py
@@ -1,5 +1,15 @@
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
-first_result = (len(s1) - len(s2) for s1, s2 in zip(first, second))
-second_result =  length_differences = (len(list1[i]) - len(list2[i]) for i in range(min(len(list1), len(list2)))) доделать дома(())))
+first_result = (len(s1) - len(s2) 
+                for s1, s2 in zip(first, second) 
+                if len(s1) != len(s2) )
+
+# second_result = (len(first[i]) - len(second[i])
+#                  for i in range(min(len(first), len(second))))
+
+second_result = (True if (len(first[i]) - len(second[i])) == 0 else False
+                 for i in range(min(len(first), len(second))))
+
+print(list(first_result))
+print(list(second_result))
